02f053618ed784883ca989a3861aab9f4f8d8a8f
Shaun Lobsinger <shaun@jolobo.com>
2021-10-17 22:48:19 -0700

    lint fixes

--- src/animation/jump.js
+++ src/animation/jump.js
@@ -1,4 +1,4 @@
-import { keyframes } from "styled-components";
+import { keyframes } from 'styled-components';
 
 export const jump = keyframes`
     from { margin-top: .5rem }
--- src/utils/fontSelector.js
+++ src/utils/fontSelector.js
@@ -1,33 +1,33 @@
 const DEFAULT_FONTS = [
-    "'Caesar Dressing', cursive",
-    "'Gugi', cursive",
-    "'IM Fell English SC', serif",
-    "'Major Mono Display', monospace",
-    "'Orbitron', sans-serif",
-    "'Press Start 2P', cursive",
-    "'Rampart One', cursive",
-    "'Source Code Pro', monospace",
-    "'UnifrakturCook', cursive",
+  "'Caesar Dressing', cursive",
+  "'Gugi', cursive",
+  "'IM Fell English SC', serif",
+  "'Major Mono Display', monospace",
+  "'Orbitron', sans-serif",
+  "'Press Start 2P', cursive",
+  "'Rampart One', cursive",
+  "'Source Code Pro', monospace",
+  "'UnifrakturCook', cursive",
 ];
 
 export const randomFontSelection = (addedFonts = []) => {
-    if (!Array.isArray(addedFonts)) throw error('must be an array of strings');
+  if (!Array.isArray(addedFonts)) throw new Error('must be an array of strings');
 
-    const combinedFonts = [...DEFAULT_FONTS, ...addedFonts];
+  const combinedFonts = [...DEFAULT_FONTS, ...addedFonts];
 
-    return combinedFonts[Math.floor(Math.random() * combinedFonts.length)];
+  return combinedFonts[Math.floor(Math.random() * combinedFonts.length)];
 };
 
 export const returnUniqueFonts = (count) => {
-    if (count > DEFAULT_FONTS.length) throw error('you have too many characters for unique characters');
+  if (count > DEFAULT_FONTS.length) throw new Error('you have too many characters for unique characters');
 
-    const fonts = [];
+  const fonts = [];
 
-    while(fonts.length < count) {
-        const selectedFont = randomFontSelection();
+  while (fonts.length < count) {
+    const selectedFont = randomFontSelection();
 
-        if (!fonts.find((item) => item === selectedFont)) fonts.push(selectedFont);
-    }
+    if (!fonts.find((item) => item === selectedFont)) fonts.push(selectedFont);
+  }
 
-    return fonts;
+  return fonts;
 };
